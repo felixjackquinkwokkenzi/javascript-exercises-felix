a79e3cf7a782e43d257dcdd01682a6dd47318b5e
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-22 05:58:08 +0700

    Complete 07_reverseString exercise

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
+const reverseString = function (string) {
+  let temp = "";
+  for (let i = string.length - 1; i >= 0; i--) {
+    temp += string[i];
+  }
 
+  return temp;
 };
 
 // Do not edit below this line
