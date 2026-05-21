bb3cd7e4bc43e058b42d5e9df366dcff09bf6baf
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-22 05:53:09 +0700

    Complete 06_repeatString exercise

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function (string, repetition) {
+  if (repetition < 0) {
+    return "ERROR";
+  }
 
+  let hasil = "";
+  for (let i = 1; i <= repetition; i++) {
+    hasil += string;
+  }
+
+  return hasil;
 };
 
 // Do not edit below this line
