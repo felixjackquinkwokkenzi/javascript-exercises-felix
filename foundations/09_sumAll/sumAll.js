e1877cced7a847b757579419de338c67897ad9ac
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-22 19:47:43 +0700

    Complete 09_sumAll exercise

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+  if (
+    a < 0 ||
+    !Number.isInteger(a) ||
+    Number.isNaN(a) ||
+    b < 0 ||
+    !Number.isInteger(b) ||
+    Number.isNaN(b)
+  )
+    return "ERROR";
 
+  let total = 0;
+
+  if (a > b) {
+    let temp = a;
+    a = b;
+    b = temp;
+  }
+
+  for (let i = a; i <= b; i++) {
+    total += i;
+  }
+
+  return total;
 };
 
 // Do not edit below this line
