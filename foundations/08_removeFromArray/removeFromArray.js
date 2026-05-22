4e68c34e5012a8c3aa0a983fe1398c1a466eea23
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-22 19:32:23 +0700

    Complete 08_removeFromArray exercise

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...itemsToRemove) {
+  let temp = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (!itemsToRemove.includes(arr[i])) {
+      temp.push(arr[i]);
+    }
+  }
+
+  return temp;
 };
 
 // Do not edit below this line
