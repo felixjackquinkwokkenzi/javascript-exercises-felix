2801e202169aca5252d5de77e71bc2579cf3cde8
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-05-22 20:19:22 +0700

    Complete 11_tempConversion exercise

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,11 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperature) {
+  const value = ((temperature - 32) * 5) / 9;
+  return Number(value.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperature) {
+  const value = (temperature * 9) / 5 + 32;
+  return Number(value.toFixed(1));
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
