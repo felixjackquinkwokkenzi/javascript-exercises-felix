ef538e37a5db64b86cc2360f008dd7daa3882659
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-06-16 20:52:51 +0700

    Complete 12_calculator exercise

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	const total = arr.reduce(function (acc, num){
+    return acc + num;
+  }, 0);
+
+  return total;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  const total = arr.reduce(function (acc, num) {
+    return acc * num;
+  }, 1);
 
+  return total;
 };
 
-const power = function() {
-	
+const power = function(base, exponent) {
+	return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  if(num === 0) return 1;
+
+  const result = num * factorial(num - 1);
+  return result;
 };
 
 // Do not edit below this line
