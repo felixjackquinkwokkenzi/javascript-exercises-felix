4f165b6c84877c28c2813d9ae503aec2e5f52537
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-06-17 23:51:46 +0700

    Complete 13_palindromes exercise

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arr =  string.toLowerCase().split("");
 
+    arr = arr.filter(function(word){
+        if(word >= "a" && word <= "z" || word >= "0" && word <= "9"){
+            return word;
+        };
+    })
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] !== arr[arr.length - i - 1]){
+            return false
+        }
+    }
+
+    return true;
 };
 
 // Do not edit below this line
