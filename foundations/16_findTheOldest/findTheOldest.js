150b53f7fbd4644f6137028426bdd1af053ac386
felixjackquinkwokkenzi <kwokfelix2@gmail.com>
2026-06-19 23:08:01 +0700

    Complete 16_findTheOldest exercise

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const getAge = function(birth, death){
+    if(!death){
+        death = new Date().getFullYear();
+    }
 
+    return death - birth;
+}
+
+const findTheOldest = function (arr) {
+  let oldest = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    const current = getAge(arr[i].yearOfBirth, arr[i].yearOfDeath);
+
+    if (current > oldest) {
+      oldest = current;
+    }
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const age = getAge(arr[i].yearOfBirth, arr[i].yearOfDeath);
+
+    if(age === oldest){
+        return arr[i];
+    }
+  }
 };
 
 // Do not edit below this line
